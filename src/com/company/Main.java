c53cfe5770915bf69d44b6c39ac074ad83f47f94
Alex Hand <alex.j.hand@gmail.com>
2018-03-03 11:07:59 -0600

    writing another method to display array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//	    int[] myIntArray = {1,2,3,4,5,6,7,8,9,10};
-//	    myIntArray[5] = 50;
-        int[] myIntArray = new int[10];
-//        double[] myDoubleArray = new double[10];
-//        System.out.println(myIntArray[0]);
-//        System.out.println(myIntArray[6]);
-//        System.out.println(myIntArray[8]);
-        for (int i = 0; i < myIntArray.length; i++) {
-            myIntArray[i] = i * 10;
-        }
-        for (int i=0; i<10; i++) {
-            System.out.println("Element " + i + ", value is " + myIntArray[i]);
-        }
+////	    int[] myIntArray = {1,2,3,4,5,6,7,8,9,10};
+////	    myIntArray[5] = 50;
+//        int[] myIntArray = new int[25];
+////        double[] myDoubleArray = new double[10];
+////        System.out.println(myIntArray[0]);
+////        System.out.println(myIntArray[6]);
+////        System.out.println(myIntArray[8]);
+//        for (int i = 0; i < myIntArray.length; i++) {
+//            myIntArray[i] = i * 10;
+//        }
+//        printArray(myIntArray);
+
+        
+
     }
+
+//    public static void printArray(int[] array) {
+//        for (int i=0; i<array.length; i++) {
+//            System.out.println("Element " + i + ", value is " + array[i]);
+//        }
+//    }
 }
