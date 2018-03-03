f79c4e210e43a09192224908120fe2b25a143646
Alex Hand <alex.j.hand@gmail.com>
2018-03-03 11:16:56 -0600

    Use arrays to find average

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner scanner = new Scanner(System.in); 
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 ////	    int[] myIntArray = {1,2,3,4,5,6,7,8,9,10};
@@ -19,8 +19,33 @@ public class Main {
 //        }
 //        printArray(myIntArray);
 
+        int[] myIntegers = getIntegers(5);
+        for (int i=0; i<myIntegers.length; i++) {
+            System.out.println("Element " + i + ", typed value as " + myIntegers[i]);
+        }
 
+        System.out.println("The average is " + getAverage(myIntegers));
 
+    }
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for (int i=0; i<values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for (int i=0; i<array.length; i++) {
+            sum += array[i];
+
+        }
+        return (double) sum / (double)array.length;
     }
 
 //    public static void printArray(int[] array) {
@@ -28,4 +53,5 @@ public class Main {
 //            System.out.println("Element " + i + ", value is " + array[i]);
 //        }
 //    }
+
 }
